acf076c03e8cf032ff058d8a3f9800816165c8ee
umang-yadav-123 <yumang680@gmail.com>
2026-06-11 12:16:16 +0530

    Data-ingestion

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys
-import logging
+from src.loggers import logging
 def error_message_details(error, error_detail: sys):
     _,_,exc_tb=error_detail = sys.exc_info()
     file_name=exc_tb.tb_frame.f_code.co_filename
