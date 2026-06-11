8ceb296b1f319129418439c0c11ca5b09b78b9e8
umang-yadav-123 <yumang680@gmail.com>
2026-06-11 14:09:37 +0530

    Data-Tansformation

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,37 +1,69 @@
 import os
 import sys
-from src.exception import customException
-from src.loggers import logging
+from dataclasses import dataclass
+
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from dataclasses import dataclass
+
+from src.exception import customException
+from src.loggers import logging
+from src.components.data_transformation import DataTransformation
+
+
 @dataclass
 class DataIngestionConfig:
-    train_data_path: str=os.path.join("artifacts","train.csv")
-    test_data_path: str=os.path.join("artifacts","test.csv")
-    raw_data_path: str=os.path.join("artifacts","data.csv")
+    train_data_path: str = os.path.join("artifacts", "train.csv")
+    test_data_path: str = os.path.join("artifacts", "test.csv")
+    raw_data_path: str = os.path.join("artifacts", "data.csv")
+
 
 class DataIngestion:
     def __init__(self):
         self.ingestion_config = DataIngestionConfig()
 
     def initiate_data_ingestion(self):
-        logging.info("Entered the data ingestion method or component")
+        logging.info("Entered the data ingestion component")
+
         try:
-            df = pd.read_csv('notebook/data/stud.csv')
-            logging.info("Read the dataset as dataframe")
+            # Read dataset
+            df = pd.read_csv("notebook/data/stud.csv")
+            logging.info("Dataset read successfully as dataframe")
 
-            os.makedirs(os.path.dirname(self.ingestion_config.train_data_path), exist_ok=True)
+            # Create artifacts directory
+            os.makedirs(
+                os.path.dirname(self.ingestion_config.train_data_path),
+                exist_ok=True
+            )
 
-            df.to_csv(self.ingestion_config.raw_data_path, index=False, header=True)
+            # Save raw data
+            df.to_csv(
+                self.ingestion_config.raw_data_path,
+                index=False,
+                header=True
+            )
 
-            logging.info("Train test split initiated")
-            train_set, test_set = train_test_split(df, test_size=0.2, random_state=42)
+            logging.info("Train-test split initiated")
 
-            train_set.to_csv(self.ingestion_config.train_data_path, index=False, header=True)
-            test_set.to_csv(self.ingestion_config.test_data_path, index=False, header=True)
+            train_set, test_set = train_test_split(
+                df,
+                test_size=0.2,
+                random_state=42
+            )
 
-            logging.info("Ingestion of the data is completed")
+            # Save train and test datasets
+            train_set.to_csv(
+                self.ingestion_config.train_data_path,
+                index=False,
+                header=True
+            )
+
+            test_set.to_csv(
+                self.ingestion_config.test_data_path,
+                index=False,
+                header=True
+            )
+
+            logging.info("Data ingestion completed successfully")
 
             return (
                 self.ingestion_config.train_data_path,
@@ -42,6 +74,20 @@ class DataIngestion:
             logging.info("Exception occurred in the data ingestion component")
             raise customException(e, sys)
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     obj = DataIngestion()
-    obj.initiate_data_ingestion()
+
+    train_data, test_data = obj.initiate_data_ingestion()
+
+    data_transformation = DataTransformation()
+
+    train_arr, test_arr, preprocessor_path = (
+        data_transformation.initiate_data_transformation(
+            train_data,
+            test_data
+        )
+    )
+
+    print("Data transformation completed successfully.")
+    print("Preprocessor saved at:", preprocessor_path)
--- src/exception.py
+++ src/exception.py
@@ -1,11 +1,15 @@
 import sys
 from src.loggers import logging
-def error_message_details(error, error_detail: sys):
-    _,_,exc_tb=error_detail = sys.exc_info()
-    file_name=exc_tb.tb_frame.f_code.co_filename
-    error_message = "Error occured in script name [{0}] line number [{1}] error message [{2}]".format(
-        file_name, exc_tb.tb_lineno, str(error)
 
+
+def error_message_details(error, error_detail: sys):
+    _, _, exc_tb = error_detail.exc_info()
+
+    return (
+        f"Error occurred in script name "
+        f"[{exc_tb.tb_frame.f_code.co_filename}] "
+        f"line number [{exc_tb.tb_lineno}] "
+        f"error message [{str(error)}]"
     )
 
 
@@ -16,12 +20,3 @@ class customException(Exception):
 
     def __str__(self):
         return self.error_message
-    
-
-if __name__=="__main__":
-    try:
-        a=1/0
-    except Exception as e:
-        logging.info("Divide by zero error")
-        raise customException(e, sys)
-    
