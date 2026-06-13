c22deb27ab6cc327c0c74bcb1ade35aa733bf09c
umang-yadav-123 <yumang680@gmail.com>
2026-06-14 02:39:35 +0530

    Hyperparamete training

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -56,6 +56,37 @@ class ModelTrainer:
                 "XGBoost Regressor": XGBRegressor(),
                 "CatBoost Regressor": CatBoostRegressor(verbose=False)
             }
+            params = {
+                "DecisionTree": {
+                    'criterion': ['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                },
+                "RandomForest": {
+                    'n_estimators': [8, 16, 32, 64, 128, 256]
+                },
+                "GradientBoosting": {
+                    'learning_rate': [0.1, 0.01, 0.05, 0.001],
+                    'subsample': [0.6, 0.7,0.75, 0.8, 0.85, 0.9],
+                    'n_estimators': [8, 16, 32, 64, 128, 256]
+                },
+                "LinearRegression": {},
+                "KNeighborsRegressor": {
+                    'n_neighbors': [5, 7, 9, 11],
+                },
+                "XGBRegressor": {
+                    'learning_rate': [0.1, 0.01, 0.05, 0.001],
+                    'n_estimators': [8, 16, 32, 64, 128, 256]
+                },
+                "CatBoostRegressor": {
+                    'depth': [6, 8, 10],
+                    'learning_rate': [0.01, 0.05, 0.1],
+                    'iterations': [30, 50, 100]
+                },
+                "AdaBoostRegressor": {
+                    'learning_rate': [0.1, 0.01, 0.5, 0.001],
+                    'n_estimators': [8, 16, 32, 64, 128, 256]
+                }
+            }
+
 
             model_report = {}
 
@@ -94,3 +125,28 @@ class ModelTrainer:
 
         except Exception as e:
             raise customException(e, sys)
+        
+def evaluate_models(X_train, y_train, X_test, y_test, models, params):
+    try:
+        model_report = {}
+
+        for model_name, model in models.items():
+            gs=GridSearchCV(
+                estimator=model,
+                param_grid=params[model_name],
+                cv=3,
+                n_jobs=-1,
+                verbose=2
+            )
+            #model.fit(X_train, y_train)
+
+            y_test_pred = model.predict(X_test)
+
+            test_model_score = r2_score(y_test, y_test_pred)
+
+            model_report[model_name] = test_model_score
+
+        return model_report
+
+    except Exception as e:
+        raise customException(e, sys)
